cf12af3a485c37c2db0ab44b0eccd4ff3ae3f7e3
wkdguswns6 <wkdguswns6@gmail.com>
2025-05-18 15:05:11 +0900

    마크다운 스타일 정리리

--- components/MarkdownContent.vue
+++ components/MarkdownContent.vue
@@ -14,4 +14,26 @@ const compiledMarkdown = computed(() => marked(props.rawMarkdown));
   </div>
 </template>
 
-<style lang="scss" scope></style>
+<style lang="scss" scope>
+.markdown-content {
+  padding: 8px;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    border-bottom: 1px solid #eaecef;
+    margin-bottom: 10px;
+  }
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: 20px;
+  }
+}
+</style>
--- pages/main/project/nuxt.vue
+++ pages/main/project/nuxt.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
-import markdownContent from '~/README.md?raw';
+import markdownContent from '~/public/Nuxt.md?raw';
 </script>
 
 <template>
   <div>
-    <h1>nuxt page</h1>
     <MarkdownContent :rawMarkdown="markdownContent"></MarkdownContent>
   </div>
 </template>
 
-<style lang="scss" scope>
-.markdown-content {
-  border-radius: 5px;
-  background-color: #f7f6f3;
-  padding: 8px;
-}
-</style>
+<style lang="scss" scope></style>
