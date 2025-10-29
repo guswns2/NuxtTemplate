f3b68b373561f2a5a8504cb5fa89a20aff84db95
wkdguswns6 <wkdguswns6@gmail.com>
2025-10-29 20:49:37 +0900

    git 컴포넌트 경로 수정

--- pages/main/project/vcs/git.vue
+++ pages/main/project/vcs/git.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import markdownContent from '~/public/markdown/Project/SCM/Git.md?raw';
+import markdownContent from '~/public/markdown/Project/VCS/Git.md?raw';
 </script>
 
 <template>
